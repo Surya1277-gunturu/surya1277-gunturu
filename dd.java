a810d24f2f2dd52f373e86037065b57ec68a5334
Surya1277-gunturu <23pa1a1277@vishnu.edu.in>
2025-07-29 16:13:43 +0530

    Create dd.java

---
+++ dd.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print a message
+        System.out.println("Hello, Java!");
+
+        // Basic addition
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        // Print the result
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
